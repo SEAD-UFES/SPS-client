c5ca50d5bffdcb8c8ef698d9a4408857fa4a4209
annecchini <fernando.void@gmail.com>
2019-04-15 17:23:20 -0300

    minor fix

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,16 +1,3 @@
 //Error actions
 export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
-
-//courses actions
-export { GET_COURSES, COURSES_LOADING } from "../components/course/courseActionTypes";
-
-//assignments actions
-export { GET_ASSIGNMENTS, ASSIGNMENTS_LOADING } from "../components/assignment/assignmentActionTypes";
-
-// //Post actions
-// export const POST_LOADING = "POST_LOADING";
-// export const GET_POSTS = "GET_POSTS";
-// export const GET_POST = "GET_POST";
-// export const ADD_POST = "ADD_POST";
-// export const DELETE_POST = "DELETE_POST";
