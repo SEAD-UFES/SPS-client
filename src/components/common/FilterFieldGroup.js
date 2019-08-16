2dee343facd3cda710864d99f05895a0a2634f03
Fernando Lyrio Annecchini <fernando.void@gmail.com>
2019-08-16 15:55:19 -0300

    wrap text on FilterFieldGroup

--- src/components/common/FilterFieldGroup.js
+++ src/components/common/FilterFieldGroup.js
@@ -1,71 +1,70 @@
-import React from "react";
+import React from 'react'
 // import classnames from "classnames";
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
 
 class FilterFieldGroup extends React.Component {
   constructor() {
-    super();
+    super()
 
-    this.onCheck = this.onCheck.bind(this);
+    this.onCheck = this.onCheck.bind(this)
   }
 
   onCheck(e) {
-    this.props.onChange(this.props.id, e.target.name);
+    this.props.onChange(this.props.id, e.target.name)
   }
 
   render() {
-    const { label, items } = this.props;
+    const { label, items } = this.props
 
     return (
       <div className="form-group dropdown cq-dropdown" data-name="statuses">
         <button
-          className="btn btn-info btn-sm dropdown-toggle form-control form-control-lg"
+          className="btn btn-info btn-sm dropdown-toggle form-control form-control-lg text-truncate"
           type="button"
           id="btndropdown"
           data-toggle="dropdown"
           aria-haspopup="true"
-          aria-expanded="true"
-        >
-          {label}:{" "}
+          aria-expanded="true">
+          {label}:{' '}
           {items.find(it => {
-            return it.marked === true;
+            return it.marked === true
           })
             ? items
-              .filter(item => {
-                return item.marked === true;
-              })
-              .map((item, key) => {
-                return (
-                  <span className="badge badge-info mr-1" key={key}>
-                    {item.label}
-                  </span>
-                );
-              })
-            : ""}
+                .filter(item => {
+                  return item.marked === true
+                })
+                .map((item, key) => {
+                  return (
+                    <span className="badge badge-info mr-1" key={key}>
+                      {item.label}
+                    </span>
+                  )
+                })
+            : ''}
           <span className="caret" />
         </button>
         <ul className="dropdown-menu" aria-labelledby="btndropdown">
           {items.length > 0
             ? items.map((item, key) => {
-              return (
-                <li className="dropdown-item" key={key}>
-                  <label className="radio-btn">
-                    <input type="checkbox" name={item.value} checked={item.marked} onChange={this.onCheck} /> {item.label}
-                  </label>
-                </li>
-              );
-            })
-            : ""}
+                return (
+                  <li className="dropdown-item" key={key}>
+                    <label className="radio-btn">
+                      <input type="checkbox" name={item.value} checked={item.marked} onChange={this.onCheck} />{' '}
+                      {item.label}
+                    </label>
+                  </li>
+                )
+              })
+            : ''}
         </ul>
       </div>
-    );
+    )
   }
 }
 
-
 FilterFieldGroup.defaultProps = {
   items: []
-};
+}
 
 FilterFieldGroup.propTypes = {
   label: PropTypes.string.isRequired
@@ -75,6 +74,6 @@ FilterFieldGroup.propTypes = {
   //   error: PropTypes.string,
   //   onChange: PropTypes.func.isRequired,
   //   options: PropTypes.array.isRequired
-};
+}
 
-export default FilterFieldGroup;
+export default FilterFieldGroup
