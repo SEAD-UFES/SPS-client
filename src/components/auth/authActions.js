caac7d89c3c6e9cd30ff75116fef1bf5a43ab612
Fernando Lyrio Annecchini <fernando.void@gmail.com>
2019-10-16 10:03:20 -0300

    prettier authActions

--- src/components/auth/authActions.js
+++ src/components/auth/authActions.js
@@ -1,71 +1,71 @@
-import spsApi from "apis/spsServer";
+import spsApi from 'apis/spsServer'
 
-import setAuthToken from "../../utils/setAuthToken";
-import jwt_decode from "jwt-decode";
+import setAuthToken from '../../utils/setAuthToken'
+import jwt_decode from 'jwt-decode'
 
-import { GET_ERRORS } from "../../actions/types";
-import { SET_CURRENT_USER } from "./authActionTypes";
+import { GET_ERRORS } from '../../actions/types'
+import { SET_CURRENT_USER } from './authActionTypes'
 
-import { clearCurrentProfile } from "components/profile/profileActions";
+import { clearCurrentProfile } from 'components/profile/profileActions'
 
 //Register
 export const registerUser = (userData, history) => dispatch => {
   spsApi
-    .post("/v1/register", userData)
-    .then(res => history.push("/login"))
+    .post('/v1/register', userData)
+    .then(res => history.push('/login'))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
       })
-    );
-};
+    )
+}
 
 //Login
 export const loginUser = userData => dispatch => {
   spsApi
-    .post("/v1/auth", userData)
+    .post('/v1/auth', userData)
     .then(res => {
-      const { access_token } = res.data;
+      const { access_token } = res.data
 
       //Set token on local storage
-      localStorage.setItem("jwtToken", access_token);
+      localStorage.setItem('jwtToken', access_token)
 
       //set token for axios to send requests with (Autorization = token) header
-      setAuthToken(access_token);
+      setAuthToken(access_token)
 
       //decode token to get user data && dispatch action to set user
-      const decoded = jwt_decode(access_token);
+      const decoded = jwt_decode(access_token)
 
-      dispatch(setCurrentUser(decoded));
+      dispatch(setCurrentUser(decoded))
     })
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
       })
-    );
-};
+    )
+}
 
 //Set User on auth
 export const setCurrentUser = decoded => {
   return {
     type: SET_CURRENT_USER,
     payload: decoded
-  };
-};
+  }
+}
 
 //Log user out
 export const logoutUser = () => dispatch => {
   //remove token from local storage
-  localStorage.removeItem("jwtToken");
+  localStorage.removeItem('jwtToken')
 
   //Remove auth header from axios future requets
-  setAuthToken(false);
+  setAuthToken(false)
 
   //set current user to {}
-  dispatch(setCurrentUser({}));
+  dispatch(setCurrentUser({}))
 
   //clear profile data
-  clearCurrentProfile();
-};
+  clearCurrentProfile()
+}
