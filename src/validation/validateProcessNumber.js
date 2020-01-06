c941cda8a9a7a9d8f61291b3f760d08810913cc2
Fernando Lyrio Annecchini <fernando.void@gmail.com>
2020-01-06 09:13:25 -0300

    process number now allow string

--- src/validation/validateProcessNumber.js
+++ src/validation/validateProcessNumber.js
@@ -1,21 +1,21 @@
-import Validator from "validator";
-import isEmpty from "./is-empty";
+import Validator from 'validator'
+import isEmpty from './is-empty'
 
 // Name validation
 export const validateProcessNumber = number => {
-  number = !isEmpty(number) ? number : "";
-  let error;
+  number = !isEmpty(number) ? number : ''
+  let error
 
-  if (!Validator.matches(number, /^\d{3}$/)) {
-    error = "Formato inválido. Deve estar no formato 000";
-  }
+  // if (!Validator.matches(number, /^\d{3}$/)) {
+  //   error = "Formato inválido. Deve estar no formato 000";
+  // }
 
   if (Validator.isEmpty(number)) {
-    error = "Este campo é requerido.";
+    error = 'Este campo é requerido.'
   }
 
   return {
     error: error,
     isValid: isEmpty(error)
-  };
-};
+  }
+}
