a83574252549563f022588eff43d14b1fc86e7b8
annecchini <fernando.void@gmail.com>
2020-06-02 10:22:25 -0300

    calendar read page draft

--- src/containers/calendar/CalendarReadContainer.js
+++ src/containers/calendar/CalendarReadContainer.js
@@ -1,10 +1,38 @@
 /** @format */
 
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+
+import { clearErrors } from '../../store/actions/error'
+import { readCall } from '../../store/actions/call'
+import { getProcess } from '../../store/actions/process'
+import { readCalendar, readListCalendar } from '../../store/actions/calendar'
 
 import CalendarRead from '../../components/calendar/CalendarRead'
 
+import { selectCalendarById, selectBrotherCalendarById } from '../../store/selectors/calendar'
+import { selectCallByCalendarId } from '../../store/selectors/call'
+import { selectProcessByCalendarId } from '../../store/selectors/process'
+
 const CalendarReadContainer = props => {
+  const id = props.match.params.id
+  const { clearErrors, readCalendar, readListCalendar, readCall, getProcess } = props
+
+  //ComponentDidMount
+  useEffect(() => {
+    clearErrors()
+    readCalendar(id, {
+      callbackOk: cld => {
+        readListCalendar({ call_ids: [cld.call_id] })
+        readCall(cld.call_id, {
+          callbackOk: call => {
+            getProcess(call.selectiveProcess_id)
+          }
+        })
+      }
+    })
+  }, [])
+
   const allProps = {
     ...props
   }
@@ -12,4 +40,27 @@ const CalendarReadContainer = props => {
   return <CalendarRead {...allProps} />
 }
 
-export default CalendarReadContainer
+const mapStateToProps = (state, ownProps) => {
+  const calendar_id = ownProps.match.params.id
+  return {
+    errorStore: state.errorStore,
+    calendars: selectBrotherCalendarById(state, calendar_id, {}),
+    calendar: selectCalendarById(state, calendar_id, { withCalendarStatus: true }),
+    calendarLoading: state.calendarStore.loading,
+    call: selectCallByCalendarId(state, calendar_id, {}),
+    process: selectProcessByCalendarId(state, calendar_id, { withCourse: true })
+  }
+}
+
+const mapActionsToProps = {
+  clearErrors,
+  readCalendar,
+  readListCalendar,
+  readCall,
+  getProcess
+}
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(CalendarReadContainer)
