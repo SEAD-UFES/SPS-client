921b9d612f6a1705792da4c04f7132136c097f63
annecchini <fernando.void@gmail.com>
2020-09-21 11:53:29 -0300

    minor fix

--- src/components/inscriptionEvent/InscriptionEventRead.js
+++ src/components/inscriptionEvent/InscriptionEventRead.js
@@ -38,7 +38,7 @@ const InscriptionEventRead = props => {
         </Link>
 
         <i className='fas fa-greater-than' />
-        <Link to={`/processes/${process ? process.id : null}`} className='breadcrumb-link'>
+        <Link to={`/processes/read/${process ? process.id : null}`} className='breadcrumb-link'>
           {process ? `Edital ${process.number}/${process.year}` : 'Edital'}
         </Link>
 
